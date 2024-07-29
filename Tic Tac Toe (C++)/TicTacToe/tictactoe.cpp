aed07c6c6cc45ecd7da50bb6a93a7bc3c86645ba
leeyanleryan <leeyanleryan.21@gmail.com>
2024-07-29 10:30:50 +0800

    tictactoe ui works, now time for game logic

--- Tic Tac Toe (C++)/TicTacToe/tictactoe.cpp
+++ Tic Tac Toe (C++)/TicTacToe/tictactoe.cpp
@@ -1,12 +1,16 @@
 #include "tictactoe.h"
 #include "ui_tictactoe.h"
+#include "QRandomGenerator"
+
+QStringList pieces;
+QStringList playerNames;
 
 TicTacToe::TicTacToe(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::TicTacToe)
 {
+    pieces = {"X", "O"};
     ui->setupUi(this);
-
     ui->stackedWidget->setCurrentIndex(0);
 }
 
@@ -37,16 +41,41 @@ void TicTacToe::on_buttonAIBack_clicked()
 
 void TicTacToe::on_buttonPlayFriend_clicked()
 {
+    int turn = ui->comboFriendFirst->currentIndex();
+    QString player1Name = ui->txtP1Name->text();
+    QString player2Name = ui->txtP2Name->text();
+    playerNames << player1Name << player2Name;
+    if (turn == 2) {
+        turn = QRandomGenerator::global()->bounded(2);
+    }
+    player1Name += ": " + pieces[turn];
+    player2Name += ": " + pieces[1-turn];
+    ui->txtP1Tile->setText(player1Name);
+    ui->txtP2Tile->setText(player2Name);
+    ui->txtCurrentTurn->setText("It's " + playerNames[turn] + "'s turn!");
     ui->stackedWidget->setCurrentIndex(2);
 }
 
 void TicTacToe::on_buttonGameBack_clicked()
 {
     ui->stackedWidget->setCurrentIndex(0);
+    playerNames = {};
 }
 
 void TicTacToe::on_buttonPlayAI_clicked()
 {
+    int turn = ui->comboAIFirst->currentIndex();
+    QString playerName = ui->txtPName->text();
+    QString AIName = ui->txtAIName->text();
+    playerNames << playerName << AIName;
+    if (turn == 2) {
+        turn = QRandomGenerator::global()->bounded(2);
+    }
+    playerName += ": " + pieces[turn];
+    AIName += ": " + pieces[1-turn];
+    ui->txtP1Tile->setText(playerName);
+    ui->txtP2Tile->setText(AIName);
+    ui->txtCurrentTurn->setText("It's " + playerNames[turn] + "'s turn!");
     ui->stackedWidget->setCurrentIndex(2);
 }
 
